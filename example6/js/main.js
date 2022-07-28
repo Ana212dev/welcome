6fe53915b775010775c7719014bcf5ee164d9861
Ana212dev <scorpionbleu5639@gmail.com>
2022-07-28 00:14:48 -0400

    redlight

---
+++ example6/js/main.js
@@ -0,0 +1,13 @@
+function updateLight(current) {
+  
+    //your code here!
+    if (current === 'red') {
+    return 'green';
+  } if (current === 'yellow') {
+    return 'red'
+  } if (current === 'green') {
+    return'yellow'
+  } else {
+   return;
+  }
+}
